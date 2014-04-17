297504d4d1a177b4a96515466848ed66218439eb
ramseyboy <walker.hannan@willowtreeapps.com>
2014-04-17 13:40:12 -0400

    initial commit

---
+++ src/main/resources/results.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+
+
+class TestSpec extends Specification {
+
+    def setupSpec() {
+
+    }
+
+    def cleanupSpec() {
+
+    }
+
+    def """In the todolist section, when the user completes n amount of tasks the todos list
+        should reflect that with the correct number of complete tasks and the correct number
+         of tasks all together
+
+if some of the todo's tasks are complete, the return if complete method should return appropriately
+
+if all of the todo's tasks are incomplete, the return if complete method should return appropriately"""() {
+
+    }
+}
