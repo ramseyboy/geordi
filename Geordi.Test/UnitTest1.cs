437075d2afe8e79438a88da8365216e7bfccb3ff
Walker Hannan <awahannan@gmail.com>
2024-09-26 22:36:12 -0500

    Re-init project as C# net skeleton

---
+++ Geordi.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Geordi.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- src/geordi/models.clj
+++
@@ -1,7 +0,0 @@
-(ns geordi.models)
-
-(defrecord Spec [spec, features])
-
-(defrecord Feature [feature, block])
-
-(defrecord Block [given, when, then])
